d22fa26bee99168fbc6620ff0ebacb4df2a91d01
Max Faber <62971602+Mfab11@users.noreply.github.com>
2023-05-25 13:35:32 -0700

    Update memSim.py

--- memSim.py
+++ memSim.py
@@ -1,18 +1,26 @@
 import argparse
-    
+
+
 class TLB:
     def __init__(self):
-        self.maxSize = 16
-        self.currentSize = 0
+        self.table = [None] * 256
         self.queue = []
+        self.physMem = 16
+        self.size = 0
 
-    def getAddress(self, virtualAddress):
-        for i in self.queue:
-            if i.virtual == virtualAddress:
-                return i
-    
-    def insert(self):
-        pass
+    def getFrame(self, address):
+        pageNum = address // 256
+        if table[pageNum] == 1:
+            return True
+        if self.size < self.physMem:
+            table[pageNum] = 1
+            self.size += 1
+        else:
+            evictNum = self.queue.pop(0)
+            table[evictNum] = 0
+        self.queue.append(pageNum)
+        return False
+
 
 class PageTable:
     def __init__(self, algo, physMem):
@@ -37,17 +45,7 @@ class PageTable:
             self.queue.append(pageNum)
             return False
 
-        if self.algo == "LRU":
-            if self.size < self.physMem:
-                table[pageNum] = 1
-                self.size += 1
-            else:
-                evictNum = self.queue.pop(0)
-                table[evictNum] = 0
-            self.queue.append(pageNum)
-            return False
-
-        if self.algo == "OPT":
+        if self.algo == "OPT" or self.algo == "LRU":
             if self.size < self.physMem:
                 table[pageNum] = 1
                 self.size += 1
@@ -66,27 +64,23 @@ class PageTable:
             return False
 
 
-
-
-
-
-
 class BackingStore:
     def __init__(self):
         self.maxSize = 65536
         self.blockSize =256
         self.data = []
+        self.readfromfile()
 
     def readfromfile(self):
         # Read the binary file
         with open('BACKING_STORE.bin', 'rb') as file:
             for _ in range(self.blockSize):
                 array_data = file.read(self.blockSize)
-                array = [byte for byte in array_data]
-                self.data.append(array)
+                self.data.append([byte for byte in array_data])
 
-    def getBlock(self, address):
-        return address // self.blockSize
+    def getData(self, address):
+        block = data[address // self.blockSize]
+        return block, block[address % self.blockSize]
 
 
 
@@ -96,15 +90,22 @@ def main():
 
     # add arguments for the job file, algorithm, and quantum
     parser.add_argument('refFile', metavar='job-file.txt', type=str,
-                        help='the file containing the list of jobs')
+                        help='list of addresses')
 
     parser.add_argument('frames', metavar='FRAMES', type=int,
-                        help='the scheduling algorithm to use', default=256)
+                        help='number of frames in physical memory', default=256)
 
     parser.add_argument('pra', metavar='PRA', type=str, default="FIFO",
-                        help='the quantum for the round-robin algorithm')
+                        help='algorithm for page table replacement')
     args = parser.parse_args()
     print(args.pra, args.frames, args.refFile)
+
+    backingStore = BackingStore()
+    pageTable = PageTable(args.pra, args.frames)
+    tlb = TLB()
+
+
+
 
 
 if __name__ == '__main__':
